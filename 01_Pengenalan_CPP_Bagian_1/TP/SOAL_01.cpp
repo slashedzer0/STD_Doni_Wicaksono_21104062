71fc459fde2d39680c4e8e08007ac62879adbce3
slashedzer0 <21104062@ittelkom-pwt.ac.id>
2024-09-28 16:55:16 +0700

    upload TP

---
+++ 01_Pengenalan_CPP_Bagian_1/TP/SOAL_01.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    string nama, nim;
+    cout << "Siapa nama Anda?";
+    cin >> nama;
+
+    cout << "Berapa NIM Anda? ";
+    cin >> nim;
+
+    cout << "Nama saya:" << nama << endl;
+    cout << "NIM saya:" << nim << endl;
+    return 0;
+}
